b42a8a0cd7a803d7debecdcc1452a8bafbcefb45
Willy Chen <terepic@gmail.com>
2016-08-18 18:46:09 -0400

    Initial Commit

---
+++ GameOfLife/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameOfLife
+//
+//  Created by Willy Chen on 8/18/16.
+//  Copyright (c) 2016 Willy Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
